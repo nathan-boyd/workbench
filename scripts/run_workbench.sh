4a4a803c0516a158048992a48738e9c1a6dbb07b
Nathan Boyd <nboyd@salesforce.com>
2020-06-05 10:55:34 -0400

    restore zsh history

--- scripts/run_workbench.sh
+++ scripts/run_workbench.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPODIR="$(dirname "$SCRIPTDIR")"
+GIT_USER_NAME=$(git config user.name)
+GIT_USER_EMAIL=$(git config user.email)
 
 PROJECT_DIR=${PWD##*/}
 PROJECT_NAME=${PWD#"${PWD%/*/*}/"}
 CONTAINER_NAME=${PROJECT_NAME//\//_}
 
-TMUX_RESURRECT=${HOME}/.ide/${PROJECT_NAME}/tmux/resurrect
+PROJECT_ZSH=${HOME}/.workbench/${PROJECT_NAME}/zsh/
+ZSH_HISTORY=${ZSH}/.zsh_history
+if [[ ! -e $ZSH_HISTORY ]]; then
+    mkdir -p ${PROJECT_ZSH}
+    touch $ZSH_HISTORY
+fi
+
+TMUX_RESURRECT=${HOME}/.workbench/${PROJECT_NAME}/tmux/resurrect
 mkdir -p ${TMUX_RESURRECT}
 
-PROJECT_ZSH=${HOME}/.workbench/${PROJECT_NAME}/zsh/
-mkdir -p ${PROJECT_ZSH}
-touch ${ZSH}/zsh_history
-
-GIT_USER_NAME=$(git config user.name)
-GIT_USER_EMAIL=$(git config user.email)
-
-# todo dynamic .kube and .helm
-#
-
 CONTAINER_HOME="/home/me"
-
 docker run \
     --rm \
     -it \
@@ -32,14 +28,12 @@ docker run \
     -v ${TMUX_RESURRECT}:$CONTAINER_HOME/.tmux/resurrect \
     -v ${ZSH_HISTORY}:$CONTAINER_HOME/.zsh_history \
     -v $HOME/.kube:$CONTAINER_HOME/.kube \
-    -e $HOME/.helm:$CONTAINER_HOME/.helm \
     -e HOST_PATH=$PWD \
     -e HOST_USER_ID=$(id -u $USER) \
     -e HOST_GROUP_ID=$(id -g $USER) \
     -e PROJECT_NAME=$PROJECT_NAME \
     -e GIT_USER_NAME="$GIT_USER_NAME" \
     -e GIT_USER_EMAIL="$GIT_USER_EMAIL" \
-    -e HELM_HOME="$HOME/.helm" \
     --name $CONTAINER_NAME \
     --net host \
     nathan-boyd/workbench:latest
