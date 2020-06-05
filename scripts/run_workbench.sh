e8e7014d93c9255e2311c5a21e3242afaaa51bff
Nathan Boyd <nboyd@salesforce.com>
2020-06-05 14:17:36 +0000

    add kubectl and mount .kube

--- scripts/run_workbench.sh
+++ scripts/run_workbench.sh
@@ -10,14 +10,17 @@ CONTAINER_NAME=${PROJECT_NAME//\//_}
 TMUX_RESURRECT=${HOME}/.ide/${PROJECT_NAME}/tmux/resurrect
 mkdir -p ${TMUX_RESURRECT}
 
-ZSH=${HOME}/.workbench/${PROJECT_NAME}/zsh/
-mkdir -p ${ZSH}
+PROJECT_ZSH=${HOME}/.workbench/${PROJECT_NAME}/zsh/
+mkdir -p ${PROJECT_ZSH}
 touch ${ZSH}/zsh_history
 
 GIT_USER_NAME=$(git config user.name)
 GIT_USER_EMAIL=$(git config user.email)
 
 # todo dynamic .kube and .helm
+#
+
+CONTAINER_HOME="/home/me"
 
 docker run \
     --rm \
@@ -25,16 +28,17 @@ docker run \
     -w /${PROJECT_DIR} \
     -v $PWD:/${PROJECT_DIR} \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v ~/.ssh:/home/me/.ssh \
-    -v ${TMUX_RESURRECT}:/home/me/.tmux/resurrect \
-    -v ${ZSH_HISTORY}:/home/me/.zsh_history \
+    -v ~/.ssh:$CONTAINER_HOME/.ssh \
+    -v ${TMUX_RESURRECT}:$CONTAINER_HOME/.tmux/resurrect \
+    -v ${ZSH_HISTORY}:$CONTAINER_HOME/.zsh_history \
+    -v $HOME/.kube:$CONTAINER_HOME/.kube \
+    -e $HOME/.helm:$CONTAINER_HOME/.helm \
     -e HOST_PATH=$PWD \
     -e HOST_USER_ID=$(id -u $USER) \
     -e HOST_GROUP_ID=$(id -g $USER) \
     -e PROJECT_NAME=$PROJECT_NAME \
     -e GIT_USER_NAME="$GIT_USER_NAME" \
     -e GIT_USER_EMAIL="$GIT_USER_EMAIL" \
-    -e KUBE_HOME="$HOME/.kube" \
     -e HELM_HOME="$HOME/.helm" \
     --name $CONTAINER_NAME \
     --net host \
